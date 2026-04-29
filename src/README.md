f1bbe51a6f0834d622f5f95e3b215d27d3e76aea
elisalindstrom <elisalindstroem@gmail.com>
2026-04-29 20:09:06 +0200

    Lagt till startsida och funktioner för att skriva ut och ta bort data från API

---
+++ src/README.md
@@ -0,0 +1,8 @@
+# DT207G Labb 3 Frontend
+Applikation som hämtar data från en webbtjänst som hanterar arbetserfarenheter.
+Det går förutom att hämta data även att lägga till ny data via ett formulär samt radera data.
+
+## Länkar
+Webbtjänst som används: https://dt207g-lab3.onrender.com/workexperience
+
+Länk till applikationen:
--- src/main.js
+++ src/main.js
@@ -1,1 +1,68 @@
 import './style.css'
+
+fetchWorkexperiences();
+
+// Hämta data från API
+async function fetchWorkexperiences() {
+    try {
+        const response = await fetch("https://dt207g-lab3.onrender.com/workexperience");
+        const workexperience = await response.json();
+
+        if (!workexperience) return;
+
+        console.log(workexperience); // Ta bort
+
+        displayWorkexperiences(workexperience);
+    } catch (error) {
+        console.error("Något gick fel:" + error);
+    }
+}
+
+// Skriv ut lista på arbetserfarenheter
+function displayWorkexperiences(workexperience) {
+    const cvList = document.querySelector("#cv-list");
+    cvList.innerHTML = "";
+
+    workexperience.forEach(experience => {
+        const liEl = document.createElement("li");
+        const titleEl = document.createElement("span");
+        titleEl.classList.add("bold");
+        const companyEl = document.createElement("span");
+        const dateEl = document.createElement("span");
+        const startdate = new Date(experience.startdate).toLocaleDateString();
+        let enddate = experience.enddate;
+
+        // Kontroll av enddate
+        if (enddate) {
+            enddate = new Date(experience.enddate).toLocaleDateString();
+        } else {
+            enddate = "Pågående";
+        }
+
+        titleEl.textContent = `${experience.jobtitle}`;
+        companyEl.textContent = `${experience.companyname}`;
+        dateEl.textContent = `${startdate} - ${enddate}`;
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Ta bort";
+
+        // Lägg till eventlistener
+        deleteBtn.addEventListener("click", () => {
+            deleteWorkexperience(experience._id);
+        });
+
+        liEl.append(titleEl, companyEl, dateEl, deleteBtn);
+        cvList.appendChild(liEl);
+    });
+}
+
+async function deleteWorkexperience(id) {
+    try {
+        const response = await fetch(`https://dt207g-lab3.onrender.com/workexperience/${id}`, {
+            method: "DELETE"
+        });
+        fetchWorkexperiences();
+    } catch (error) {
+        console.error("Något gick fel:" + error);
+    }
+}
